d2a0915f68807ede7b0e7b02f6b4ddf0ad1a5e2c
Ahire Prayag <prayagahire365@gmail.com>
2026-07-02 15:42:08 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// IP : a unique identifier assigning to every device on a network, allowing computer to communicate with each other.
+// Port : a communication endpoint that allows multiple applications to use the same IP address for network communication.
+// It is a 16-bit number that ranges from 0 to 65_535, with certain ports reserved for specific services (e.g., HTTP uses port 80, HTTPS uses port 443).
+// TCP (Transmission Control Protocol) : a connection-oriented protocol that ensures reliable data transmission between devices on a network.
+//  It establishes a connection before data transfer and guarantees that data is delivered in the correct order without loss or duplication.
+// socket : an endpoint for communication between two devices on a network, combining an IP address and a port number to uniquely identify a specific service or application on a device.
+
+std::net::TcpListener
+
+
+fn main() { 
+    println!("Hello, world!");
+}
